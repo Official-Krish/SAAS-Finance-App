98157dc9f3e31069282c2871de521288725d0136
Official-Krish <krishanand0103@gmail.com>
2024-05-30 17:50:09 +0530

    added accounts-get api

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,16 +1,25 @@
 import { Hono } from "hono";
 import { db } from "@/db/drizzle";
 import { accounts } from "@/db/schema";
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { eq } from "drizzle-orm";
+
 
 const app = new Hono()
-   .get("/", async (c) => {
-    const data = await db 
+   .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) {
+      return c.json ({error : "unauthorized"}, 401 )
+    }
+
+    const data = await db
       .select({
-        id : accounts.id,
-        name : accounts.name,
+        id: accounts.id,
+        name: accounts.name,
       })
-      .from(accounts);
-    return c.json ({data});
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+    return c.json({ data });
 });
 
 export default app;
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,10 +2,10 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from  "./accounts"
 
-
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
+
 const routes = app
    .route ("/accounts", accounts)
 
