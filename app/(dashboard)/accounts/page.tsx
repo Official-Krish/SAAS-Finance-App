f0c0d601ed19c69cded2be343d79aca8e08556b3
Official-Krish <krishanand0103@gmail.com>
2024-05-31 16:47:36 +0530

    added accounts page

---
+++ app/(dashboard)/accounts/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Card , CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { Plus } from "lucide-react";
+
+const AccountPage = () => {
+    const newAccount = useNewAccount();
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <Card className="border-none drop-shadow-sm">
+                <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+                    <CardTitle className="text-xl line-clamp-1">
+                        Accounts Page
+                    </CardTitle>
+                    <Button size="sm" onClick={newAccount.onOpen}>
+                        <Plus className="size-4 mr-2"/>
+                        Add new
+                    </Button>
+                </CardHeader>
+            </Card>
+        </div>
+    )
+}
+
+export default AccountPage;
