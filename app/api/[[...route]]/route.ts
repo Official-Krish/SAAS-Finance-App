b2ac263d31f33b489f2c6aaa87479c02551d452b
Official-Krish <krishanand0103@gmail.com>
2024-06-09 16:22:26 +0530

    Added transactions routes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import accounts from  "./accounts"
 import categories  from './categories';
+import transactions from './transactions';
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
@@ -9,6 +10,7 @@ const app = new Hono().basePath('/api')
 const routes = app
    .route ("/accounts", accounts)
    .route ("/categories", categories)
+   .route ("/transactions", transactions)
 
 export const GET = handle(app);
 export const POST = handle(app);
